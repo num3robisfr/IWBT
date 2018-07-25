ddf2f91270470d90c0e2cbe004e6990abe31714c
thierry <thierry@DESKTOP-QA4KQRF>
2018-07-25 14:57:06 +0200

    methode connexion

--- src/java/Model/beanOeuvre.java
+++ src/java/Model/beanOeuvre.java
@@ -192,39 +192,4 @@ public class beanOeuvre implements Serializable {
         return "Titre = " + oeuTitre ;
     }
     
-    /********************************************************************************************************************/
-    
-    public ArrayList<beanOeuvre> remplirListeOeuvres(Connection connexion, String filtre, String recherche) {
-        
-        ArrayList<beanOeuvre> liste=new ArrayList();
-        try {
-            String query = "SELECT * FROM ListesOeuvres2";
-            Statement stmt = connexion.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-
-                beanOeuvre o = new beanOeuvre(rs.getString("ISBN"),
-                        rs.getString("Titre"),
-                        rs.getString("Sous Titre"),
-                        rs.getString("Auteur"),
-                        rs.getString("Editeur"),
-                        rs.getString("Date de parution"),
-                        rs.getString("Resume"),
-                        new Integer(rs.getInt("Nbre de page")),
-                        rs.getString("Image"),
-                        rs.getString("Theme"),
-                        rs.getString("Sous Theme"),
-                        new Float(rs.getFloat("Prix")),
-                        rs.getString("Tva"),
-                        rs.getString("Nbre de cmt"),
-                        rs.getString("Moyenne des notes"),
-                        rs.getString("Statut"));
-                liste.add(o);            }
-            rs.close();
-            stmt.close();
-        } catch (SQLException ex) {
-            System.out.println("Oops:SQL:" + ex.getMessage());
-        } return liste;
-    }
 }
