935f8dbc5acb05f73b2273dde40f6331c9560944
cdi312 <cdi312@pltz845.AFPA-PARIS-DI>
2018-07-26 15:11:57 +0200

    modif

---
+++ src/java/classe/ItemV2.java
@@ -0,0 +1,43 @@
+package classe;
+
+public class ItemV2 {
+    
+    private String urlImage;
+    private String ref;
+    private int qty;
+
+    public ItemV2(String urlImage, String ref, int qty) {
+        this.urlImage = urlImage;
+        this.ref = ref;
+        this.qty = qty;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+    
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    
+    public void change( int qty) {
+        this.qty+= qty;
+    }
+    
+}
