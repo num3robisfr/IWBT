14bfe4c531126a228a67143fc35ab99fce259a16
cdi312 <cdi312@pltz845.AFPA-PARIS-DI>
2018-08-02 16:15:23 +0200

    modif beanPanier

--- src/java/Model/beanPanier.java
+++ src/java/Model/beanPanier.java
@@ -8,38 +8,42 @@ import java.util.HashMap;
 public class beanPanier implements Serializable {
     
     private HashMap<String, Item> map = null;
-    
+    Item i = null;
+
     public beanPanier() {
         this.map = new HashMap();
     }
-    
-//    public void add(String ref) {
-//        add(null, ref, null, 1);        
-//    }
-    
-    public void add(String urlImage, String ref, String titre) {
-        add(urlImage, ref, titre, 1);
+
+    public void add(String urlImage, String ref, String titre, Double prix) {
+        add(urlImage, ref, titre, prix, 1);
     }
-    
-    public void add(String urlImage, String ref, String titre, String qty) {
+
+    public void add(String urlImage, String ref, String titre, String prix) {
         try {
-            int q = Integer.parseInt(qty);
-            add(urlImage, ref, titre, q);
+            Double p = Double.parseDouble(prix);
+            add(urlImage, ref, titre, p, 1);
         } catch (NumberFormatException e) {
             System.out.println("Oops:" + e.getMessage());
         }
     }
-    
-    public void add(String urlImage, String ref, String titre, int qty) {
-        Item i = null;
+
+    public void add(String urlImage, String ref, String titre, String prix, String qty) {
+        try {
+            Double p = Double.parseDouble(prix);
+            int q = Integer.parseInt(qty);
+            add(urlImage, ref, titre, p, q);
+        } catch (NumberFormatException e) {
+            System.out.println("Oops:" + e.getMessage());
+        }
+    }
+
+    public void add(String urlImage, String ref, String titre, Double prix, int qty) {
+
         if (map.containsKey(ref)) {
             i = map.get(ref);
-//            i = map.get(urlImage);
-//            i = map.get(ref);
-//            i = map.get(titre);
             i.change(qty);
         } else {
-            i = new Item(urlImage, ref, titre, qty);
+            i = new Item(urlImage, ref, titre, prix, qty);
             map.put(ref, i);
         }
         if (i.getQty() < 1) {
@@ -47,45 +51,58 @@ public class beanPanier implements Serializable {
         }
         System.out.println(i);
     }
-    
-    public void dec(String urlImage, String ref, String titre) {
-        add(urlImage, ref, titre, -1);
+
+    public void dec(String urlImage, String ref, String titre, String prix) {
+        Double p = Double.parseDouble(prix);
+        add(urlImage, ref, titre, p, -1);
     }
-    
-    public void dec(String urlImage, String ref, String titre, int qty) {
-        add(urlImage, ref, titre, -qty);
+
+    public void dec(String urlImage, String ref, String titre, Double prix, int qty) {
+        add(urlImage, ref, titre, prix, -qty);
     }
-    
-    
+
 //    public void dec(String urlImage, String ref, String titre, int qty) {
+//        Item i = null;
 //        if (map.containsKey(ref)) {
-//        Item i = map.get(ref);
+//        i = map.get(ref);
 //            i.minus(qty);
+//        }if (i.getQty() < 1) {
+//            del(ref);
 //        }
 //    }
-    
     public void del(String ref) {
         map.remove(ref);
+        // total = total - i.getSomme();
     }
-    
-    
+
     public void clean() {
         map.clear();
     }
-    
+
     public boolean isEmpty() {
         return map.isEmpty();
     }
-    
+
     public int size() {
         return map.size();
     }
-    
-    public void save() {
-    }
-    
+
     public Collection<Item> getList() {
         return map.values();
     }
+
+    public Double getTotal(String ref) {
+        Double total = 0.0;
+
+        if (!this.isEmpty()) {
+            for (Item i : getList()) {
+                total += i.getSomme();
+            }
+        }
+        System.out.println("+++++++++++++" + total);
+        return total;
+    }
+
     
+
 }
